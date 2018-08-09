d1d42065f60d42ab1f7ee5c6c5e6b7a4861a28cd
Bofei Zhu <zhu.bofei@gmail.com>
2018-08-08 23:45:05 -0400

    Finished two sum

---
+++ 1-two-sum.playground/Contents.swift
@@ -0,0 +1,45 @@
+/// 1. Two Sum
+/// Given an array of integers, return indices of the two numbers such that they add up to a
+/// specific target.
+///
+/// You may assume that each input would have exactly one solution, and you may not use the same
+/// element twice.
+
+import XCTest
+
+/// Approach: Two-pass Dictionary
+func twoSum(_ nums: [Int], for target: Int) -> [Int] {
+    // 1st pass build a dictionary for number -> index
+    var dict: [Int: Int] = [:]
+    for (idx, num) in nums.enumerated() {
+        dict[num] = idx
+    }
+    
+    // 2nd pass
+    for (idx, num) in nums.enumerated() {
+        let complement = target - num
+        guard let complementIdx = dict[complement] else { continue }
+        return [idx, complementIdx]
+    }
+    
+    // each input would have exactly one solution
+    fatalError()
+}
+
+class Tests: XCTestCase {
+    func test0() {
+        let nums = [2, 7, 11, 15]
+        let target = 9
+        let solution = twoSum(nums, for: target)
+        XCTAssertEqual(solution, [0, 1])
+    }
+    
+    func test1() {
+        let nums = [2, 7, 11, 15, 23]
+        let target = 34
+        let solution = twoSum(nums, for: target)
+        XCTAssertEqual(solution, [2, 4])
+    }
+}
+
+Tests.defaultTestSuite.run()
