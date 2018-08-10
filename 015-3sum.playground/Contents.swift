20224785fc8c6904339e97f73284d49c6bdf00da
Bofei Zhu <zhu.bofei@gmail.com>
2018-08-10 16:09:49 -0400

    Update Contents.swift

--- 015-3sum.playground/Contents.swift
+++ 015-3sum.playground/Contents.swift
@@ -1,4 +1,4 @@
-/// 16. 3Sum
+/// 15. 3Sum
 /// Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
 /// Find all unique triplets in the array which gives the sum of zero.
 ///
